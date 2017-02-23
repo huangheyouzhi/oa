40d4afd1de895ca637d152117050e2a88c4bcfc1
developerli <developerli@126.com>
2017-02-23 18:07:47 +0800

    test

---
+++ oa/src/main/java/com/hhyz/oa/dao/UserMapper.java
@@ -0,0 +1,16 @@
+package com.hhyz.oa.dao;
+
+import com.hhyz.oa.model.User;
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String userId);
+
+    int insert(User record);
+
+    User selectByPrimaryKey(String userId);
+
+    List<User> selectAll();
+
+    int updateByPrimaryKey(User record);
+}
